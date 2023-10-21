dcadf76babf241cddd16a53acceae77ad5fc40db
Sidharth v nair <122723720+Sidharthzidzid@users.noreply.github.com>
2023-10-21 10:47:44 +0530

    Create Binary-search-tree.py

---
+++ Binary-search-tree.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if key < root.val:
+            root.left = insert(root.left, key)
+        else:
+            root.right = insert(root.right, key)
+    return root
+
+def search(root, key):
+    if root is None or root.val == key:
+        return root
+    if root.val < key:
+        return search(root.right, key)
+    return search(root.left, key)
+
+def inorder_traversal(root):
+    if root:
+        inorder_traversal(root.left)
+        print(root.val)
+        inorder_traversal(root.right)
+
+root = Node(50)
+root = insert(root, 30)
+root = insert(root, 20)
+root = insert(root, 40)
+root = insert(root, 70)
+root = insert(root, 60)
+root = insert(root, 80)
+
+print("Inorder Traversal of BST:")
+inorder_traversal(root)
+
+result = search(root, 40)
+if result:
+    print("\nFound 40 in the BST.")
+else:
+    print("\n40 not found in the BST.")
